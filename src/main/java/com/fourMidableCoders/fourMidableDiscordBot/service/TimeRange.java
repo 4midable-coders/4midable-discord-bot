2edbac8e483dceda4f1dc9527d436625a48bbd79
Peter Liebhart <peterliebhart.business@gmail.com>
2024-01-18 17:11:14 +0100

    just added some comments and removed the 2 getter methods in TimeRange, because they are never used and it is easy to access them with dot notation anyway.

--- src/main/java/com/fourMidableCoders/fourMidableDiscordBot/service/TimeRange.java
+++ src/main/java/com/fourMidableCoders/fourMidableDiscordBot/service/TimeRange.java
@@ -3,7 +3,6 @@ package com.fourMidableCoders.fourMidableDiscordBot.service;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 
 
 //This class is used to calculate the time range for the calendar events.
@@ -27,23 +26,29 @@ public class TimeRange {
     public TimeRange(TimeRangeType rangeType) {
         //Coordinated Universal Time (UTC) is used as the time zone for the time range.
         ZonedDateTime now = ZonedDateTime.now(ZoneId.systemDefault());
+        //The switch statement is used to determine the start and end of the time range.
         switch (rangeType) {
+            //The start is set to the current time and the end is set to the current time plus one day to represent the time range of today.
             case TODAY:
                 this.start = now;
                 this.end = this.start.plusDays(1);
                 break;
+            //The start is set to the current time plus one day and the end is set to the current time plus two days to represent the time range of tomorrow.
             case TOMORROW:
                 this.start = now.plusDays(1);
                 this.end = this.start.plusDays(1);
                 break;
+            //The start is set to the current time and the end is set to the current time plus one week to represent the time range of the next 7 days.
             case WEEK:
                 this.start = now;
                 this.end = this.start.plusWeeks(1);
                 break;
+            //The start is set to the current time and the end is set to the current time plus one year to represent the time range of the next 365 days.
             case YEAR:
                 this.start = now;
                 this.end = this.start.plusYears(1);
                 break;
+            //The start is set to the current time and the end is set to the current time plus one month to represent the time range of the next 30 days.
             case MONTH:
                 this.start = now;
                 this.end = this.start.plusMonths(1);
@@ -51,14 +56,6 @@ public class TimeRange {
             default:
                 throw new IllegalArgumentException("Invalid time range type");
         }
-    }
-
-    public ZonedDateTime getStart() {
-        return start;
-    }
-
-    public ZonedDateTime getEnd() {
-        return end;
     }
 
     //These methods return the start and end of the time range as a formatted string. This is needed for the Google Calendar API to work properly.
